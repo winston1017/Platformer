d5a72911bd3f686d54a8157f6d189110d6f44a19
winston1017 <winston1017@gmail.com>
2017-05-28 00:29:52 -0700

    Added platforms

---
+++ Assets/Scripts/CollisionTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionTrigger : MonoBehaviour {
+
+	private BoxCollider2D playerCollider;
+
+	[SerializeField]
+	private BoxCollider2D platformCollider;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
