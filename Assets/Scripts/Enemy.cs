34086e7896dc0857aaf87b0b4c4487c9914318a8
winston1017 <winston1017@gmail.com>
2017-06-01 23:48:56 -0700

    Added enemy and controller

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    private IEnemyState currentState;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ChangeState(IEnemyState newState)
+    {
+        if (currentState != null)
+        {
+            currentState.Exit();
+        }
+
+        currentState = newState;
+        //currentState.Enter
+    }
+}
---
+++ Assets/Scripts/EnemyStates/IEnemyState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IEnemyState
+{
+
+    void Execute();
+    void Enter(Enemy enemy);
+    void Exit();
+    void OnTriggerEnter2D(Collider2D other);
+
+
+
+	
+}
